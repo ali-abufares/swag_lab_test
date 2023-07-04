63a75796fe891060a165dc39d96cdb5707802928
ali-abufares <aliabufares1993@gmail.com>
2023-07-04 16:30:31 +0300

    update

---
+++ cypress/e2e/test_1.js
@@ -0,0 +1,43 @@
+//< reference type ="cypress"/>
+//beforeEach( ()=> {
+//cy.clearAllCookies()
+//cy.clearAllLocalStorage()
+//cy.clearAllSessionStorage()
+//cy.log('cach and cookies cleared')
+//cy.reload()
+//}
+//)
+describe('test login',() =>{
+    it('test log in - standed user',()=>{
+       //cy.visit("https://www.google.com/")
+       // cy.visit("https://www.saucedemo.com/").visit()
+        
+
+    // for(let i=0 ;i<8 ;i++){
+        // cy.get(["button.list-group-item"])
+
+    // }
+    
+    f
+       // cy.get('["button.list-group-item"]').click()
+       // cy.get('[data-test="username"]').type("standard_user")
+        //cy.get('[data-test="password"]').type("secret_sauce")
+        //cy.get('[data-test="login-button"]').click()
+        //cy.get("button.btn_inventory").click({multipel:true})
+        //cy.get("button#add-to-cart-sauce-labs-backpack").click({multipel:true})
+
+    //    for(let i=0;i<6;i++){
+      //      cy.get("dev#inventory_container").click()
+
+   // }
+//cy.contains("Add to cart").click()
+
+
+
+    });
+    it('log_in test', () => {
+        cy.visit("https://www.c.com/")
+
+        
+    });
+});
